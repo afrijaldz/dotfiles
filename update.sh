7a83deabfa3fbb053517856c04366f8bd1bb829a
Afrijal Dzuhri <ijuldz@gmail.com>
2022-12-28 17:09:31 +0700

    update dotfiles

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+git add .
+git commit -m "update dotfiles"
+git push
