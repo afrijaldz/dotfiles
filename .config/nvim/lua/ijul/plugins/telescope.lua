46c1e2d52e7cd0d18578d92827f3d6d78884df33
Afrijal Dzuhri <ijuldz@gmail.com>
2023-01-19 15:56:06 +0700

    update dotfiles

--- .config/nvim/lua/ijul/plugins/telescope.lua
+++ .config/nvim/lua/ijul/plugins/telescope.lua
@@ -16,7 +16,32 @@ telescope.setup({
         ["<C-j>"] = actions.move_selection_next,
         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
       }
-    }
+    },
+    preview = {
+      mime_hook = function(filepath, bufnr, opts)
+        local is_image = function(filepath)
+          local image_extensions = {'png','jpg'}   -- Supported image formats
+          local split_path = vim.split(filepath:lower(), '.', {plain=true})
+          local extension = split_path[#split_path]
+          return vim.tbl_contains(image_extensions, extension)
+        end
+        if is_image(filepath) then
+          local term = vim.api.nvim_open_term(bufnr, {})
+          local function send_output(_, data, _ )
+            for _, d in ipairs(data) do
+              vim.api.nvim_chan_send(term, d..'\r\n')
+            end
+          end
+          vim.fn.jobstart(
+            {
+              'catimg', filepath  -- Terminal image viewer command
+            }, 
+            {on_stdout=send_output, stdout_buffered=true})
+        else
+          require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
+        end
+      end
+    },
   }
 })
 
