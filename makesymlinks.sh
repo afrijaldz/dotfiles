d4c032e1d2750362ca6076bc2247f468245c022e
Afrijal Dzuhri <ijuldz@gmail.com>
2024-01-10 12:38:37 +0700

    update nvim 2024

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,9 +6,9 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc vscode oh-my-zsh tmux.conf"    # list of files/folders to symlink in homedir
+dir=~/dotfiles                                                        # dotfiles directory
+olddir=~/dotfiles_old                                                 # old dotfiles backup directory
+files="bashrc vimrc vim zshrc vscode oh-my-zsh tmux.conf config/nvim" # list of files/folders to symlink in homedir
 
 ##########
 
@@ -22,13 +22,13 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/.$file ~/.$file
+	echo "Moving any existing dotfiles from ~ to $olddir"
+	mv ~/.$file ~/dotfiles_old
+	echo "Creating symlink to $file in home directory."
+	ln -s $dir/.$file ~/.$file
 done
 
 curl -fLo ~/dotfiles/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
